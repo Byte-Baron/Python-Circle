816aa2aa08c4d21e7a66535c61e0cb03baac7e3c
Stan <86017139+Byte-Baron@users.noreply.github.com>
2024-07-21 22:24:37 +0330

    Add files via upload

---
+++ Circle.py
@@ -0,0 +1,9 @@
+radius =float(input("Enter circle Radius: "))
+
+perimeter= radius*2*3.141592
+area= radius*radius*3.141592
+
+#Chord_Length
+a=float(radius*radius*2)
+chord_length= a** (1/2)
+print("Circle Perimeter is :",perimeter,"   ","Circle Area is :",area,"   ","Circle Chord-Length is :",chord_length)
