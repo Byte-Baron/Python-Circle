5ada57040f1203c3800ce454e210162fd0abf160
Stan <86017139+Byte-Baron@users.noreply.github.com>
2024-07-22 20:00:23 +0330

    Update Circle.py

--- Circle.py
+++ Circle.py
@@ -1,9 +1,10 @@
+print()
 radius =float(input("Enter circle Radius: "))
+distance =float(input("Enter the distance from the cilecle center to the chord: "))
 
-perimeter= radius*2*3.141592
-area= radius*radius*3.141592
+perimeter= radius*2*3.14
+area= radius*radius*3.14
 
 #Chord_Length
-a=float(radius*radius*2)
-chord_length= a** (1/2)
+chord_length = 2*(radius**2 - distance**2) ** 0.5
 print("Circle Perimeter is :",perimeter,"   ","Circle Area is :",area,"   ","Circle Chord-Length is :",chord_length)
